9c3e3e690334eb7ededdfe780755a787552ef0aa
Johnatan <johnatanosterloh@gmail.com>
2022-01-11 18:34:13 -0300

    Configurações iniciais do projeto

---
+++ src/main/java/br/com/senai/CursosSenaiApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosSenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosSenaiApplication.class, args);
+	}
+
+}
